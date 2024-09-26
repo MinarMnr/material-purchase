84e58d245e7e981156dae599588df2457a956d73
minar <mahmudur@eatlbd.com>
2024-09-26 18:55:17 +0600

    login page design ongoing

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,8 @@
 "use client";
 import LoginForm from "./loginForm";
+import Image from "next/image";
+import StarImage from "../../../public/Review.png";
+import FaceImage from "../../../public/BG.png";
 
 export default function Page() {
   return (
@@ -9,45 +12,43 @@ export default function Page() {
     >
       {/* Left side */}
       <div
-        className="flex flex-col justify-center items-center bg-blue-600 text-white p-8"
+        //className="flex flex-col justify-center items-center bg-blue-600 text-white p-8"
+        className="flex bg-blue-600 text-white"
         style={{ width: "709px", height: "858px" }}
       >
         <div
           style={{
-            width: "472px",
+            width: "473px",
             height: "633.77px",
-            top: "110px",
-            left: "152px",
+            marginTop: "110px",
+            marginLeft: "152px",
           }}
         >
-          <h1 className="text-6xl font-bold mb-4">Welcome to our community</h1>
-          <p className="text-lg mb-8">
+          <h1 className="text-6xl font-semibold mb-4">
+            Welcome to our community
+          </h1>
+          <p className="text-lg mb-5 text-[#CBD5E1] mr-[35px]">
             Clarity gives you the blocks & components you need to create a truly
             professional website.
           </p>
-          <div className="flex items-center">
-            <div className="flex text-yellow-400 mb-4">
-              {/* Star Icons for Ratings */}
-              {[...Array(5)].map((_, index) => (
-                <svg
-                  key={index}
-                  className="w-6 h-6 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <polygon points="12 2 15 10 22 10 17 14 19 22 12 18 5 22 7 14 2 10 9 10 12 2" />
-                </svg>
-              ))}
-            </div>
+          <div className="flex items-center mt-[197px]">
+            <Image src={StarImage} alt="Stars" quality={100} />
           </div>
-          <p className="text-lg italic mb-4">
+
+          <p className="text-xl mt-8">
             "We love Landingfolio! Our designers were using it for their
             projects, so we already knew what kind of design they want."
           </p>
-          <p className="font-semibold">Devon Lane</p>
-          <p>Co-Founder, Design.co</p>
+
+          <div className="flex items-center space-x-4 mt-8">
+            <Image src={FaceImage} alt="Face" quality={100} />
+            <div>
+              <p className="font-semibold">Devon Lane</p>
+              <p className="font-normal text-sm text-[#CBD5E1]">
+                Co-Founder, Design.co
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
